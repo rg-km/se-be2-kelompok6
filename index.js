0b07a6fa39b9e1b4cb9c7811631c2bcc8eb0d38b
Damara Syaidil <damaraccd11@gmail.com>
2022-03-10 21:29:32 +0700

    Feat : turn snake into one

--- index.js
+++ index.js
@@ -42,9 +42,9 @@ function initSnake(color) {
     }
 }
 let snake1 = initSnake("purple");
-let snake2 = initSnake("blue");
-// Soal no 6: add snake3
-let snake3 = initSnake("black");
+// let snake2 = initSnake("blue");
+// // Soal no 6: add snake3
+// let snake3 = initSnake("black");
 
 // Soal no 4: make apples array
 let apples = [{
@@ -66,11 +66,12 @@ function drawScore(snake) {
     let scoreCanvas;
     if (snake.color == snake1.color) {
         scoreCanvas = document.getElementById("score1Board");
-    } else if (snake.color == snake2.color) {
-        scoreCanvas = document.getElementById("score2Board");
-    } else {
-        scoreCanvas = document.getElementById("score3Board");
-    }
+    } 
+    // else if (snake.color == snake2.color) {
+    //     scoreCanvas = document.getElementById("score2Board");
+    // } else {
+    //     scoreCanvas = document.getElementById("score3Board");
+    // }
     let scoreCtx = scoreCanvas.getContext("2d");
 
     scoreCtx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
@@ -91,16 +92,16 @@ function draw() {
             drawCell(ctx, snake1.body[i].x, snake1.body[i].y, snake1.color);
         }
 
-        drawCell(ctx, snake2.head.x, snake2.head.y, snake2.color);
-        for (let i = 1; i < snake2.body.length; i++) {
-            drawCell(ctx, snake2.body[i].x, snake2.body[i].y, snake2.color);
-        }
+        // drawCell(ctx, snake2.head.x, snake2.head.y, snake2.color);
+        // for (let i = 1; i < snake2.body.length; i++) {
+        //     drawCell(ctx, snake2.body[i].x, snake2.body[i].y, snake2.color);
+        // }
 
-        // Soal no 6: Draw Player 3
-        drawCell(ctx, snake3.head.x, snake3.head.y, snake3.color);
-        for (let i = 1; i < snake3.body.length; i++) {
-            drawCell(ctx, snake3.body[i].x, snake3.body[i].y, snake3.color);
-        }
+        // // Soal no 6: Draw Player 3
+        // drawCell(ctx, snake3.head.x, snake3.head.y, snake3.color);
+        // for (let i = 1; i < snake3.body.length; i++) {
+        //     drawCell(ctx, snake3.body[i].x, snake3.body[i].y, snake3.color);
+        // }
 
         for (let i = 0; i < apples.length; i++) {
             let apple = apples[i];
@@ -111,9 +112,9 @@ function draw() {
         }
 
         drawScore(snake1);
-        drawScore(snake2);
-        // Soal no 6: Draw Player 3 Score:
-        drawScore(snake3);
+        // drawScore(snake2);
+        // // Soal no 6: Draw Player 3 Score:
+        // drawScore(snake3);
     }, REDRAW_INTERVAL);
 }
 
@@ -186,8 +187,8 @@ function checkCollision(snakes) {
         audio.play();
 
         alert("Game over");
-        snake1 = initSnake("purple");
-        snake2 = initSnake("blue");
+        // snake1 = initSnake("purple");
+        // snake2 = initSnake("blue");
     }
     return isCollide;
 }
@@ -209,7 +210,8 @@ function move(snake) {
     }
     moveBody(snake);
     // Soal no 6: Check collision dengan snake3
-    if (!checkCollision([snake1, snake2, snake3])) {
+    // if (!checkCollision([snake1, snake2, snake3])) {
+    if (!checkCollision([snake1])) {
         setTimeout(function() {
             move(snake);
         }, MOVE_INTERVAL);
@@ -247,32 +249,32 @@ document.addEventListener("keydown", function (event) {
         turn(snake1, DIRECTION.DOWN);
     }
 
-    if (event.key === "a") {
-        turn(snake2, DIRECTION.LEFT);
-    } else if (event.key === "d") {
-        turn(snake2, DIRECTION.RIGHT);
-    } else if (event.key === "w") {
-        turn(snake2, DIRECTION.UP);
-    } else if (event.key === "s") {
-        turn(snake2, DIRECTION.DOWN);
-    }
+    // if (event.key === "a") {
+    //     turn(snake2, DIRECTION.LEFT);
+    // } else if (event.key === "d") {
+    //     turn(snake2, DIRECTION.RIGHT);
+    // } else if (event.key === "w") {
+    //     turn(snake2, DIRECTION.UP);
+    // } else if (event.key === "s") {
+    //     turn(snake2, DIRECTION.DOWN);
+    // }
 
-    // Soal no 6: Add navigation snake3:
-    if (event.key === "j") {
-        turn(snake3, DIRECTION.LEFT);
-    } else if (event.key === "l") {
-        turn(snake3, DIRECTION.RIGHT);
-    } else if (event.key === "i") {
-        turn(snake3, DIRECTION.UP);
-    } else if (event.key === "k") {
-        turn(snake3, DIRECTION.DOWN);
-    }
+    // // Soal no 6: Add navigation snake3:
+    // if (event.key === "j") {
+    //     turn(snake3, DIRECTION.LEFT);
+    // } else if (event.key === "l") {
+    //     turn(snake3, DIRECTION.RIGHT);
+    // } else if (event.key === "i") {
+    //     turn(snake3, DIRECTION.UP);
+    // } else if (event.key === "k") {
+    //     turn(snake3, DIRECTION.DOWN);
+    // }
 })
 
 function initGame() {
     move(snake1);
-    move(snake2);
-    move(snake3);
+    // move(snake2);
+    // move(snake3);
 }
 
 initGame();
